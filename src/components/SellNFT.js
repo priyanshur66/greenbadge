a59a214da268908b41f53911641332fe1f436f68
priyanshur66 <priyanshuranjan0001@gmail.com>
2023-09-28 17:22:41 +0530

    ui changes

--- src/components/SellNFT.js
+++ src/components/SellNFT.js
@@ -1,145 +1,226 @@
 import Navbar from "./Navbar";
 import { useState } from "react";
 import { uploadFileToIPFS, uploadJSONToIPFS } from "../pinata";
-import Marketplace from '../Marketplace.json';
+import Marketplace from "../Marketplace.json";
 import { useLocation } from "react-router";
-import './sellNFT.css';
-export default function SellNFT () {
-    const [formParams, updateFormParams] = useState({ name: '', description: '', price: ''});
-    const [fileURL, setFileURL] = useState(null);
-    const ethers = require("ethers");
-    const [message, updateMessage] = useState('');
-    const location = useLocation();
+import "./sellNFT.css";
+export default function SellNFT() {
+  const [formParams, updateFormParams] = useState({
+    name: "",
+    description: "",
+    price: "",
+  });
+  const [fileURL, setFileURL] = useState(null);
+  const ethers = require("ethers");
+  const [message, updateMessage] = useState("");
+  const location = useLocation();
 
-    async function disableButton() {
-        const listButton = document.getElementById("list-button")
-        listButton.disabled = true
-        listButton.style.backgroundColor = "grey";
-        listButton.style.opacity = 0.3;
+  async function disableButton() {
+    const listButton = document.getElementById("list-button");
+    listButton.disabled = true;
+    listButton.style.backgroundColor = "grey";
+    listButton.style.opacity = 0.3;
+  }
+
+  async function enableButton() {
+    const listButton = document.getElementById("list-button");
+    listButton.disabled = false;
+    listButton.style.backgroundColor = "#A500FF";
+    listButton.style.opacity = 1;
+  }
+
+  //This function uploads the NFT image to IPFS
+  async function OnChangeFile(e) {
+    var file = e.target.files[0];
+    //check for file extension
+    try {
+      //upload the file to IPFS
+      disableButton();
+      updateMessage("Uploading image.. please dont click anything!");
+      const response = await uploadFileToIPFS(file);
+      if (response.success === true) {
+        enableButton();
+        updateMessage("");
+        console.log("Uploaded image to Pinata: ", response.pinataURL);
+        setFileURL(response.pinataURL);
+      }
+    } catch (e) {
+      console.log("Error during file upload", e);
+    }
+  }
+
+  //This function uploads the metadata to IPFS
+  async function uploadMetadataToIPFS() {
+    const { name, description, price } = formParams;
+    //Make sure that none of the fields are empty
+    if (!name || !description || !price || !fileURL) {
+      updateMessage("Please fill all the fields!");
+      return -1;
     }
 
-    async function enableButton() {
-        const listButton = document.getElementById("list-button")
-        listButton.disabled = false
-        listButton.style.backgroundColor = "#A500FF";
-        listButton.style.opacity = 1;
+    const nftJSON = {
+      name,
+      description,
+      price,
+      image: fileURL,
+    };
+
+    try {
+      //upload the metadata JSON to IPFS
+      const response = await uploadJSONToIPFS(nftJSON);
+      if (response.success === true) {
+        console.log("Uploaded JSON to Pinata: ", response);
+        return response.pinataURL;
+      }
+    } catch (e) {
+      console.log("error uploading JSON metadata:", e);
     }
+  }
 
-    //This function uploads the NFT image to IPFS
-    async function OnChangeFile(e) {
-        var file = e.target.files[0];
-        //check for file extension
-        try {
-            //upload the file to IPFS
-            disableButton();
-            updateMessage("Uploading image.. please dont click anything!")
-            const response = await uploadFileToIPFS(file);
-            if(response.success === true) {
-                enableButton();
-                updateMessage("")
-                console.log("Uploaded image to Pinata: ", response.pinataURL)
-                setFileURL(response.pinataURL);
-            }
-        }
-        catch(e) {
-            console.log("Error during file upload", e);
-        }
+  async function listNFT(e) {
+    e.preventDefault();
+
+    //Upload data to IPFS
+    try {
+      const metadataURL = await uploadMetadataToIPFS();
+      if (metadataURL === -1) return;
+      //After adding your Hardhat network to your metamask, this code will get providers and signers
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      disableButton();
+      updateMessage(
+        "Uploading Proposal(wait for a moment).. please dont click anything!"
+      );
+
+      //Pull the deployed contract instance
+      let contract = new ethers.Contract(
+        Marketplace.address,
+        Marketplace.abi,
+        signer
+      );
+
+      //massage the params to be sent to the create NFT request
+      const price = ethers.utils.parseUnits(formParams.price, "ether");
+      let listingPrice = await contract.getListPrice();
+      listingPrice = listingPrice.toString();
+
+      //actually create the NFT
+      let transaction = await contract.createToken(metadataURL, price, {
+        value: listingPrice,
+      });
+      await transaction.wait();
+
+      alert("Successfully listed your NFTs!");
+      enableButton();
+      updateMessage("");
+      updateFormParams({ name: "", description: "", price: "" });
+      window.location.replace("/");
+    } catch (e) {
+      alert("Upload error" + e);
     }
+  }
 
-    //This function uploads the metadata to IPFS
-    async function uploadMetadataToIPFS() {
-        const {name, description, price} = formParams;
-        //Make sure that none of the fields are empty
-        if( !name || !description || !price || !fileURL)
-        {
-            updateMessage("Please fill all the fields!")
-            return -1;
-        }
-
-        const nftJSON = {
-            name, description, price, image: fileURL
-        }
-
-        try {
-            //upload the metadata JSON to IPFS
-            const response = await uploadJSONToIPFS(nftJSON);
-            if(response.success === true){
-                console.log("Uploaded JSON to Pinata: ", response)
-                return response.pinataURL;
-            }
-        }
-        catch(e) {
-            console.log("error uploading JSON metadata:", e)
-        }
-    }
-
-    async function listNFT(e) {
-        e.preventDefault();
-
-        //Upload data to IPFS
-        try {
-            const metadataURL = await uploadMetadataToIPFS();
-            if(metadataURL === -1)
-                return;
-            //After adding your Hardhat network to your metamask, this code will get providers and signers
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            const signer = provider.getSigner();
-            disableButton();
-            updateMessage("Uploading Proposal(wait for a moment).. please dont click anything!")
-
-            //Pull the deployed contract instance
-            let contract = new ethers.Contract(Marketplace.address, Marketplace.abi, signer)
-
-            //massage the params to be sent to the create NFT request
-            const price = ethers.utils.parseUnits(formParams.price, 'ether')
-            let listingPrice = await contract.getListPrice()
-            listingPrice = listingPrice.toString()
-
-            //actually create the NFT
-            let transaction = await contract.createToken(metadataURL, price, { value: listingPrice })
-            await transaction.wait()
-
-            alert("Successfully listed your NFTs!");
-            enableButton();
-            updateMessage("");
-            updateFormParams({ name: '', description: '', price: ''});
-            window.location.replace("/")
-        }
-        catch(e) {
-            alert( "Upload error"+e )
-        }
-    }
-
-    console.log("Working", process.env);
-    return (
-        <div className="">
-        <Navbar></Navbar>
-        <div className="flex flex-col place-items-center mt-10" id="nftForm">
-            <form className="form-bg shadow-md rounded px-8 pt-4 pb-8 mb-4">
-            <h3 className="text-center font-bold text-lg text-white mb-8">Upload Your Proposal</h3>
-                <div className="mb-4">
-                    <label className="block text-white text-md font-bold mb-2" htmlFor="name">Title</label>
-                    <input className="chh focus-bg" id="name" type="text" placeholder="Proposal Title" onChange={e => updateFormParams({...formParams, name: e.target.value})} value={formParams.name}></input>
-                </div>
-                <div className="mb-6">
-                    <label className="block text-white text-md font-bold mb-0 dis" htmlFor="description">Proposal Description</label>
-                    <textarea className="chh dis" cols="40" rows="5" id="description" type="text" placeholder="Write your discription...." value={formParams.description} onChange={e => updateFormParams({...formParams, description: e.target.value})}></textarea>
-                </div>
-                <div className="mb-6">
-                    <label className="block text-white text-md font-bold mb-2" htmlFor="price">Price (in ETH)</label>
-                    <input className="chh" type="number" placeholder="Min 0.001 ETH" step="0.001" value={formParams.price} onChange={e => updateFormParams({...formParams, price: e.target.value})}></input>
-                </div>
-                <div>
-                    <label className="block text-white text-md font-bold mb-2" htmlFor="image">Upload Image (&lt;500 KB)</label>
-                    <input type={"file"} onChange={OnChangeFile}></input>
-                </div>
-                <br></br>
-                <div className="text-red-500 text-center">{message}</div>
-                <button onClick={listNFT} className="font-bold mt-10 w-full bg-green-500 text-white rounded-full p-2 shadow-lg" id="list-button">
-                    List Proposal
-                </button>
-            </form>
-        </div>
-        </div>
-    )
+  console.log("Working", process.env);
+  return (
+    <div className="">
+      <Navbar></Navbar>
+      <div>
+        <a
+          className="flex flex-col place-items-center mt-10"
+          href="https://www.waterlily.ai/"
+          target="blank"
+        >
+          <button
+            type="submit"
+            class=" h-auto rounded-full bg-white text-black py-3 px-6 "
+          >
+            <span class="text-teal-900 font-semibold">
+              Generate starter images here
+            </span>
+          </button>
+        </a>
+      </div>
+      <div className="flex flex-col place-items-center mt-10" id="nftForm">
+        <form className="form-bg shadow-md rounded px-8 pt-4 pb-8 mb-4">
+          <h3 className="text-center font-bold text-lg text-white mb-8">
+            Upload Your Proposal
+          </h3>
+          <div className="mb-4">
+            <label
+              className="block text-white text-md font-bold mb-2"
+              htmlFor="name"
+            >
+              Title
+            </label>
+            <input
+              className="chh focus-bg"
+              id="name"
+              type="text"
+              placeholder="Proposal Title"
+              onChange={(e) =>
+                updateFormParams({ ...formParams, name: e.target.value })
+              }
+              value={formParams.name}
+            ></input>
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-white text-md font-bold mb-0 dis"
+              htmlFor="description"
+            >
+              Proposal Description
+            </label>
+            <textarea
+              className="chh dis"
+              cols="40"
+              rows="5"
+              id="description"
+              type="text"
+              placeholder="Write your discription...."
+              value={formParams.description}
+              onChange={(e) =>
+                updateFormParams({ ...formParams, description: e.target.value })
+              }
+            ></textarea>
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-white text-md font-bold mb-2"
+              htmlFor="price"
+            >
+              Price (in ETH)
+            </label>
+            <input
+              className="chh"
+              type="number"
+              placeholder="Min 0.001 ETH"
+              step="0.001"
+              value={formParams.price}
+              onChange={(e) =>
+                updateFormParams({ ...formParams, price: e.target.value })
+              }
+            ></input>
+          </div>
+          <div>
+            <label
+              className="block text-white text-md font-bold mb-2"
+              htmlFor="image"
+            >
+              Upload Image (&lt;500 KB)
+            </label>
+            <input type={"file"} onChange={OnChangeFile}></input>
+          </div>
+          <br></br>
+          <div className="text-red-500 text-center">{message}</div>
+          <button
+            onClick={listNFT}
+            className="font-bold mt-10 w-full bg-green-500 text-white rounded-full p-2 shadow-lg"
+            id="list-button"
+          >
+            List Proposal
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
